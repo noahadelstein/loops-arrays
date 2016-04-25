1b6411afdb5d575100983ae37164fa57791fc9f2
noahadelstein <noadelstein@gmail.com>
2016-04-25 18:36:21 -0500

    j

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ while (count < 10) {
 // output results
 document.write("<h1>Loops</h1>");
 document.write("<ul>" + list + "</ul>");
+
+var myItems = [
+  ["question 1", "answer one"],
+[" question 2" , "answer two"]
+];
+var i = 0;
+var secondList = "<ul>";
+while (i < myItems.length) {
+  secondList += "<li>" + myItems(i) + "</li>");
+  i++;
+}
+secondList+= "</ul>";
+document.write(secondList);
